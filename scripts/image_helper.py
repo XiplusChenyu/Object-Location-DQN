ec318228e1aa98a9ac27931e8f5a6111ad6d3fad
Chenyu Xi <xi_chenyu@163.com>
2018-11-30 01:03:19 -0500

    modify image_helper

--- scripts/image_helper.py
+++ scripts/image_helper.py
@@ -2,11 +2,11 @@ from keras.preprocessing import image
 import numpy as np
 
 
-def get_all_ids(annotations):
-    all_ids = []
-    for i in range(len(annotations)):
-        all_ids.append(get_ids_objects_from_annotation(annotations[i]))
-    return all_ids
+# def get_all_ids(annotations):
+#     all_ids = []
+#     for i in range(len(annotations)):
+#         all_ids.append(get_ids_objects_from_annotation(annotations[i]))
+#     return all_ids
 
 
 def get_all_images(image_names, path_voc):
@@ -29,9 +29,9 @@ def get_all_images_pool(image_names, path_voc):
 
 def load_images_names_in_data_set(data_set_name, path_voc):
     file_path = path_voc + '/ImageSets/Main/' + data_set_name + '.txt'
-    f = open(file_path)
-    image_names = f.readlines()
-    image_names = [x.strip('\n') for x in image_names]
+    with open(file_path) as f:
+        image_names = f.readlines()
+        image_names = [x.strip('\n') for x in image_names]
     if data_set_name.startswith("aeroplane") | data_set_name.startswith("bird") | data_set_name.startswith("cow"):
         return [x.split(None, 1)[0] for x in image_names]
     else:
@@ -40,10 +40,10 @@ def load_images_names_in_data_set(data_set_name, path_voc):
 
 def load_images_labels_in_data_set(data_set_name, path_voc):
     file_path = path_voc + '/ImageSets/Main/' + data_set_name + '.txt'
-    f = open(file_path)
-    images_names = f.readlines()
-    images_names = [x.split(None, 1)[1] for x in images_names]
-    images_names = [x.strip('\n') for x in images_names]
+    with open(file_path) as f:
+        images_names = f.readlines()
+        images_names = [x.split(None, 1)[1] for x in images_names]
+        images_names = [x.strip('\n') for x in images_names]
     return images_names
 
 
